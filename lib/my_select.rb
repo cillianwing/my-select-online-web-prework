fedb5747dfdb26e33ab04bd1080660bed4ab502e
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-02 18:58:28 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,11 @@
 def my_select(collection)
  # your code here!
- i = 0 
- new_collection = []
+  i = 0 
+  new_collection = []
  
- while i < collection.length 
-  
+  while i < collection.length 
+    if yield(collection[i]) == TRUE 
+      new_collection << collection[i]
+    end
+  end
 end
